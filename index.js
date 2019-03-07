ecd9c2c47655f86a0529ba18a6a78ee493938567
LAPTOP-US8N6EDV\chunh <chunhui@live.nl>
2019-03-07 14:18:14 +0100

    Small improvement with comments

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/about', (req, res) => res.send('about'))
 
+//This displays a msg in the terminal if executed with node index.js
 app.listen(port, () => console.log(`index.js app listening on port ${port}!`))
