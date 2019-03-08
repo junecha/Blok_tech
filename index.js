3d909d37c89da6d41e8230b59705a8f926b535da
LAPTOP-US8N6EDV\chunh <chunhui@live.nl>
2019-03-08 11:00:05 +0100

    some changes

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
-const port = 3000
 const app = express()
-
-app.use('/static', express.static('static'));
+const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/about', (req, res) => res.send('about'))
-
+//404 response when all the other functions and routes are responding. Always put this at the bottom.
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+})
 //This displays a msg in the terminal if executed with node index.js
-app.listen(port, () => console.log(`index.js app listening on port ${port}!`))
+app.listen(3000, function() {
+  console.log('App is running on port ' + port + '!');
+}
