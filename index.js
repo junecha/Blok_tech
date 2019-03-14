78595557d70e0331183acaac1433a80ee108aed0
Chun <45426792+ChunningTatum@users.noreply.github.com>
2019-03-14 14:37:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+var pug = require('pug')
+var port = 3000
+
+//Routing with pug
+app.use('/static', express.static('static'))
+app.set('view engine', 'pug')
+   .get('/', index)
+
+function index(req, res){
+  res.render('index.pug')
+}
+
+//This displays a msg in the terminal if executed with node index.js
+app.listen(3000, function() {
+  console.log('App is running on port ' + port + '!');
+})
