318e116f73204064b58102bc6a6d15a0a9e8f0ba
LAPTOP-US8N6EDV\chunh <chunhui@live.nl>
2019-03-14 14:48:57 +0100

    Revert "Delete index.js"
    This reverts commit 024f8dca50a3991d40319babcd7071224ddfb944.

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use('/static', express.static('public'));
+
+app.listen(port, () => console.log(`Listening app on port ${port}!`))
