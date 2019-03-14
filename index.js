8a693ffb7e6e1456f569e340de1f5097432c43eb
LAPTOP-US8N6EDV\chunh <chunhui@live.nl>
2019-03-14 14:54:52 +0100

    use pug

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var express = require('express')
+var app = express()
+var port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/static', express.static('static'))
+app.set('view engine', pug)
 
-//404 response when all the other functions and routes are responding. Always put this at the bottom.
-app.use(function (req, res, next) {
-  res.status(404).send("Sorry can't find that!")
-})
 //This displays a msg in the terminal if executed with node index.js
 app.listen(3000, function() {
   console.log('App is running on port ' + port + '!');
