bf11855a91b4b2adb529eb018cbe9dfd3e451f56
LAPTOP-US8N6EDV\chunh <chunhui@live.nl>
2019-03-15 09:10:19 +0100

    pug added

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ var express = require('express')
 var app = express()
 var port = 3000
 
+//Use pug
 app.use('/static', express.static('static'))
 app.set('view engine', pug)
+   .get('/', index)
+
+function index(req, res) {
+  res.render(index.pug)
+}
 
 //This displays a msg in the terminal if executed with node index.js
 app.listen(3000, function() {
