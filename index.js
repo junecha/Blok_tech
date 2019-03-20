ffbfa0551a6189c0ccba8dc0db7546ebcb59f260
ChunningTatum <45426792+ChunningTatum@users.noreply.github.com>
2019-03-20 13:13:12 +0100

    using ejs

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ var express = require('express')
 var app = express()
 var port = 3000
 
-//Use pug
-app.use('/static', express.static('static'))
-app.set('view engine', pug)
-   .get('/', index)
+// Use ejs
 
-function index(req, res) {
-  res.render(index.pug)
-}
 
-//This displays a msg in the terminal if executed with node index.js
+app.get('/', function(req, res){
+  res.render("index.js")
+})
+
+
+//This displays a msg in the terminal if executed with 'node index.js'
 app.listen(3000, function() {
-  console.log('App is running on port ' + port + '!');
-}
+  console.log('App is running on port ' + port + '!')
+})
