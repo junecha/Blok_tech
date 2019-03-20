eba0ff21274cd21a430a74ef53dfc5839944e050
ChunningTatum <45426792+ChunningTatum@users.noreply.github.com>
2019-03-20 16:01:16 +0100

    made ejs work, partials added, css works

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-var express = require('express')
-var app = express()
-var port = 3000
+var express = require('express');
+var port = 3000;
 
-// Use ejs
+express()
+  //'give' access to the static folder
+  .use('/static', express.static('static'))
 
+  .set('view engine', 'ejs')
+  .set('views', 'views')
 
-app.get('/', function(req, res){
-  res.render("index.js")
-})
+  //Make routes to different pages.  Method(path, handler) (Don't forget to actualy create the handler)
+  .get('/', index)
+  .get('/profile', profile)
 
+  //This listens to the var port. var port usually holds 8000 or 3000. (match localhost:'port' to the number stated at the top)
+  .listen(port)
 
-//This displays a msg in the terminal if executed with 'node index.js'
-app.listen(3000, function() {
-  console.log('App is running on port ' + port + '!')
-})
+//req = requst, res = response. This handles the index request from '.get('/', inder)'
+function index(req, res) {
+  res.render('index.ejs')
+}
+
+function profile(req, res) {
+  res.render('profile.ejs')
+}
