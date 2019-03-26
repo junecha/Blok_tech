b772c12d698e66e83ba3e2a408fb5929668d7c87
ChunningTatum <45426792+ChunningTatum@users.noreply.github.com>
2019-03-26 16:03:14 +0100

    improved files and started with mongodb

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
-var port = 3000;
+var mongo = require('mongodb');
+var bodyParser = require('body-parser');
+var port = 8000;
 
 express()
   //'give' access to the static folder
@@ -11,6 +13,10 @@ express()
   //Make routes to different pages.  Method(path, handler) (Don't forget to actualy create the handler)
   .get('/', index)
   .get('/profile', profile)
+  .get('/interest', interest)
+
+  // .get('/form-with-get.ejs', form_with_get)
+  // .get('/form-with-post', form_with_post)
 
   //This listens to the var port. var port usually holds 8000 or 3000. (match localhost:'port' to the number stated at the top)
   .listen(port)
@@ -23,3 +29,19 @@ function index(req, res) {
 function profile(req, res) {
   res.render('profile.ejs')
 }
+
+function interest(req, res) {
+  res.render('interest.ejs')
+}
+
+// function form_with_get(req, res) {
+//   return response.render('form-with-get')
+// }
+//
+// function form_with_post(req, res) {
+//   return response.render('form-with-post')
+// }
+//
+// function form_with_get(req, res) {
+//   return response.send(req.query)
+// }
