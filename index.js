5d686648d9c61367fc428eba96749decaad8ef61
ChunningTatum <45426792+ChunningTatum@users.noreply.github.com>
2019-04-24 11:44:16 +0200

    Progressed

--- index.js
+++ index.js
@@ -6,24 +6,28 @@ var port = 8000;
 express()
   //'give' access to the static folder
   .use('/static', express.static('static'))
+  .use(bodyParser.urlencoded({ extended: false}))
+  .use(bodyParser.json())
 
   .set('view engine', 'ejs')
   .set('views', 'views')
 
   //Make routes to different pages.  Method(path, handler) (Don't forget to actualy create the handler)
   .get('/', index)
+  .get('/sign-up', signUp)
   .get('/profile', profile)
   .get('/interest', interest)
 
-  // .get('/form-with-get.ejs', form_with_get)
-  // .get('/form-with-post', form_with_post)
-
   //This listens to the var port. var port usually holds 8000 or 3000. (match localhost:'port' to the number stated at the top)
-  .listen(port)
+  .listen(port, console.log("Listening on port " + port))
 
-//req = requst, res = response. This handles the index request from '.get('/', inder)'
+//req = requst, res = response. This handles the index request from '.get('/', index)'
 function index(req, res) {
   res.render('index.ejs')
+}
+
+function signUp(req, res) {
+  res.render('sign-up.ejs')
 }
 
 function profile(req, res) {
@@ -33,15 +37,3 @@ function profile(req, res) {
 function interest(req, res) {
   res.render('interest.ejs')
 }
-
-// function form_with_get(req, res) {
-//   return response.render('form-with-get')
-// }
-//
-// function form_with_post(req, res) {
-//   return response.render('form-with-post')
-// }
-//
-// function form_with_get(req, res) {
-//   return response.send(req.query)
-// }
