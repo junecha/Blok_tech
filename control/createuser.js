f5177832d142d7cf857f8cb2e7ea38483dca63c7
Chun <45426792+ChunningTatum@users.noreply.github.com>
2020-02-28 09:22:30 +0100

    Fixed database connection

---
+++ control/createuser.js
@@ -0,0 +1,37 @@
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+let db = null;
+
+const dbUri = process.env.DB_URI;
+const dbName = process.env.DB_NAME;
+const client = new MongoClient(dbUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+client.connect(err => {
+  if (err) {
+    console.log(err);
+    throw err;
+  }
+  db = client.db(dbName);
+  console.log("Connected to database")
+});
+
+
+module.exports = (req, res) => {
+  let person = {
+    name: req.body.name,
+    age: req.body.age,
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  db.collection("person").insertOne(person, (error, person) => {
+        res.redirect("/");
+    })
+};
--- server.js
+++ server.js
@@ -2,84 +2,75 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require("mongodb").ObjectId;
+const assert = require("assert");
 const dotenv = require('dotenv');
-
-const app = express();
-
+const session = require('express-session')
+const app = express()
+app.use(session({
+  'secret': 'secret1234'
+}));
 const port = 3000;
 
 dotenv.config();
 
-  app.set('view engine', 'ejs')
-  app.set('views', 'views')
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 
-  app.use(bodyParser.json())
-  app.use('/static', express.static('static'))
-  app.use(bodyParser.urlencoded( {extended: true} ))
+app.use(bodyParser.json())
+app.use('/static', express.static('static'))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
-  app.get('/', index)
-  app.get('/sign-up', signUp)
-  app.get('/log-in', logIn)
-
-  app.post('/sign-up', postForm)
-
-  app.listen(port, console.log("Listening on port " + port))
-
-
-// Intialize connection to MongoDB database
-  let db = null;
-  const dbUri = process.env.DB_URI;
-  const dbName = process.env.DB_NAME;
-  const client = new MongoClient(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-  client.connect(error => {
-    if (error) {
-      console.log(error);
-      throw error;
-    }
-    db = client.db(dbName);
-    console.log("Connected to database")
-  });
-
-  // MongoClient.connect(dbUri, function(err, client) {
-  //   db = client.db(dbName);
-  // })
 
 
 //Routing-----------------------------------------------------------------------
 
-  function index(req, res) {
-    res.render('index.ejs');
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+})
+
+app.get('/sign-up', (req, res) => {
+  res.render('sign-up.ejs')
+})
+
+app.get('/log-in', (req, res) => {
+  res.render('log-in.ejs')
+})
+
+app.listen(port, () => console.log("Listening on port " + port))
+
+
+// Intialize connection to MongoDB database-------------------------------------
+let db = null;
+const dbUri = process.env.DB_URI;
+const dbName = process.env.DB_NAME;
+const client = new MongoClient(dbUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+client.connect(err => {
+  if (err) {
+    console.log(err);
+    throw err;
   }
+  db = client.db(dbName);
+});
 
-  function signUp(req, res) {
-    res.render('sign-up.ejs');
-  }
+//'Self made' packages----------------------------------------------------------
+const createUser = require('./control/createuser.js');
 
-  function logIn(req, res) {
-    res.render('log-in.ejs');
-  }
+app.post("/sign-up", createUser);
 
-//Form posting------------------------------------------------------------------
 
-  function postForm(req, res) {
-    console.log("post received");
-
-    let personInfo = req.body;
-
-    let Person = {
-      name: personInfo.name,
-      age: personInfo.age
-    };
-    console.log("Person added");
-  }
 //Error handling----------------------------------------------------------------
 
-  app.use(function (err, req, res, next) {
-    console.error(err.stack)
-    res.status(500).send('Something broke!')
-  })
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
-  app.use(function (req, res, next) {
-    res.render('404.ejs');
-  })
+app.use((req, res, next) => {
+  res.render('404.ejs');
+})
